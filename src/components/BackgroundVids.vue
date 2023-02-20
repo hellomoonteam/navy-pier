33b706a256424b69ca3c462f570c7a37352d7223
Tom Benbow <tom@hellomoon.net>
2023-02-20 13:48:04 -0600

    Add muted attribute to video to fix auto play

--- src/components/BackgroundVids.vue
+++ src/components/BackgroundVids.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <video ref="player1" :src="player1Source" v-bind:class="{ active: player1Active == true, visible: player1Visible == true }" width="3320" height="1280" autoplay />
-    <video ref="player2" :src="player2Source" v-bind:class="{ active: player1Active == false, visible: player2Visible == true }"  width="3320" height="1280" autoplay />
+    <video ref="player1" :src="player1Source" v-bind:class="{ active: player1Active == true, visible: player1Visible == true }" width="3320" height="1280" autoplay muted />
+    <video ref="player2" :src="player2Source" v-bind:class="{ active: player1Active == false, visible: player2Visible == true }"  width="3320" height="1280" autoplay muted />
   </div>
 </template>
 
@@ -19,7 +19,7 @@ PreFade (2s before end of scene)
     Fade next up and play
 Cycle (at end of scene)
     Stop inactivePlayer
-
+    
 */
 export default {
   data: function() {
